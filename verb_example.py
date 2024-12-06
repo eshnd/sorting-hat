fbe32d1a97b801d97cc70c5f597c60f77de1146d
eshon <eshaan@Laptop-Probably.local>
2024-12-06 08:04:51 -0600

    verb example

---
+++ verb_example.py
@@ -0,0 +1,46 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import wordnet 
+import nltk
+
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+
+def derive(texts):
+    lem = WordNetLemmatizer()
+    devd = []
+    for text in texts:
+        words = text.split()
+        combined = " ".join(
+            lem.lemmatize(word.lower(), pos=wordnet.VERB) + " " +
+            lem.lemmatize(word.lower(), pos=wordnet.NOUN)
+            for word in words
+        )
+        devd.append(combined)
+    return devd
+
+def search(inn):
+    titles = ["gryffindor", "hufflepuff", "ravenclaw", "slytherin"]
+    descps = ["brave and childish", "dumb and stupid", "smart and goth", "mean and snake and doing evil things"]
+
+    derive_descps = derive(descps)
+    
+    vec = TfidfVectorizer()
+    vector_descps = vec.fit_transform(derive_descps)
+  
+    main_m = KNeighborsClassifier(n_neighbors=1)
+    main_m.fit(vector_descps, titles)
+    
+
+    def search_model(ina):
+        d_ina = derive([ina])[0] 
+        ina_vec = vec.transform([d_ina])
+        prediction = main_m.predict(ina_vec)
+        return prediction[0]
+
+    final = search_model(inn)
+    return final
+
+inp = input("Hello! Welcome to the sorting hat, describe yourself here: ")
+print(search(inp))
